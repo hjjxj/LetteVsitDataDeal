7e269984ac301d10534df2b01deddd8d007584e4
lenovo <123>
2019-04-15 19:54:06 +0800

    jquery、bootstrap

--- DataDeal/urls.py
+++ DataDeal/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from DataDealApp.views import GetTest, GetTestasd
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', GetTest),
